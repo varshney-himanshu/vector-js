0429ab733e1270f76917800a558be43f01b46f2d
Himanshu Varshney <himanshu1998delhi@gmail.com>
2023-01-22 13:21:54 +0530

    Added more methods

--- vector2d.js
+++ vector2d.js
@@ -1,43 +1,82 @@
 class Vector2D {
-    constructor(x = 0, y = 0) {
-        this._x = x;
-        this._y = y;
-    }
+  constructor(x = 0, y = 0) {
+    this._x = x;
+    this._y = y;
+  }
 
-    getX() {
-        return this._x;
-    }
+  getX() {
+    return this._x;
+  }
 
-    getY() {
-        return this._y;
-    }
+  getY() {
+    return this._y;
+  }
 
-    setX(x) {
-        this._x = x;
-    }
+  setX(x) {
+    this._x = x;
+  }
 
-    setY(y) {
-        this._y = y;
-    }
+  setY(y) {
+    this._y = y;
+  }
 
-    getAngle() {
-        return Math.atan2(this._x, this._y);
-    }
+  setAngle(a) {
+    var length = this.getLength();
+    this._x = Math.cos(a) * length;
+    this._y = Math.sin(a) * length;
+  }
 
-    getAngleInDeg() {
-        return Math.atan2(this._y, this._x) * 180 / Math.PI
-    }
+  getAngle() {
+    return Math.atan2(this._x, this._y);
+  }
 
-    setLength(length) {
-        let angle = this.getAngle();
-        this._x = length * Math.cos(angle);
-        this._y = length * Math.sin(angle);
-    }
+  getAngleInDeg() {
+    return (Math.atan2(this._y, this._x) * 180) / Math.PI;
+  }
 
-    getLength() {
-        return Math.sqrt(Math.pow(this._x, 2) + Math.pow(this._y, 2));
-    }
+  setLength(length) {
+    let angle = this.getAngle();
+    this._x = length * Math.cos(angle);
+    this._y = length * Math.sin(angle);
+  }
 
+  getLength() {
+    return Math.sqrt(Math.pow(this._x, 2) + Math.pow(this._y, 2));
+  }
+
+  add(v2) {
+    return vector.create(this._x + v2.getX(), this._y + v2.getY());
+  }
+
+  subtract(v2) {
+    return vector.create(this._x - v2.getX(), this._y - v2.getY());
+  }
+
+  multiply(val) {
+    return vector.create(this._x * val, this._y * val);
+  }
+
+  divide(val) {
+    return vector.create(this._x / val, this._y / val);
+  }
+
+  addTo(v2) {
+    this._x += v2.getX();
+    this._y += v2.getY();
+  }
+
+  subtractFrom(v2) {
+    this._x -= v2.getX();
+    this._y -= v2.getY();
+  }
+
+  multiplyBy(val) {
+    this._x *= val;
+    this._y *= val;
+  }
+
+  divideBy(val) {
+    this._x /= val;
+    this._y /= val;
+  }
 }
-
-export default Vector2D;
